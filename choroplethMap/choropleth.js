19ab184ee79c7cb0832dbfb85fd5b78bad6a5f16
InaShenLewander <mirishen1993@gmail.com>
2019-04-14 22:31:55 +0200

    Used Axios to fetch data from two urls

--- choroplethMap/choropleth.js
+++ choroplethMap/choropleth.js
@@ -1,6 +1,14 @@
-const educationUS = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
-      countyUS = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json';
+const urlUSEducation = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
+      urlUSCounty = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json',
+      responses = [];
+
+const getUSEducation = () => axios.get(urlUSEducation).then(res => responses.push(res));
+
+const getUSCounty = () => axios.get(urlUSCounty).then(res => responses.push(res));
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+     axios.all([getUSEducation(), getUSCounty()])
+        .then(axios.spread((acct, perms) => {
+            console.log(responses);
+        }))
 });
