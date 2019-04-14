49709afb89ba1ce8c5acc67714b2f5cb63a68856
InaShenLewander <mirishen1993@gmail.com>
2019-04-14 13:11:40 +0200

    Initializing choropleth map

---
+++ choroplethMap/choropleth.js
@@ -0,0 +1,6 @@
+const educationUS = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
+      countyUS = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json';
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+});
